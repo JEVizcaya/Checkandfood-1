e097da762bee85fb29ebf2bd460f89913e8d5fdf
JEVizcaya <jevizcaya@hotmail.com>
2025-04-11 19:35:15 +0200

    checkandfoodsql

--- checkandfood.sql
+++ checkandfood.sql
@@ -87,7 +87,7 @@ CREATE TABLE `reserve` (
   KEY `time_slot_id` (`time_slot_id`),
   CONSTRAINT `reserve_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `reserve_ibfk_3` FOREIGN KEY (`time_slot_id`) REFERENCES `time_slot` (`time_slot_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +96,7 @@ CREATE TABLE `reserve` (
 
 LOCK TABLES `reserve` WRITE;
 /*!40000 ALTER TABLE `reserve` DISABLE KEYS */;
-INSERT INTO `reserve` VALUES (10,8,24,4,'2025-03-27','rechazada',13),(11,8,27,3,'2025-03-29','cancelada',13),(12,8,28,4,'2025-04-03','rechazada',13),(13,8,26,2,'2025-03-28','rechazada',13),(14,8,33,5,'2025-03-28','pendiente',14);
+INSERT INTO `reserve` VALUES (15,8,50,4,'2025-04-04','cancelada',17),(16,8,55,2,'2025-04-06','cancelada',18),(17,8,71,4,'2025-04-05','cancelada',21),(18,8,73,3,'2025-04-04','rechazada',21),(19,8,45,2,'2025-04-25','aceptada',17),(20,8,45,2,'2025-04-10','pendiente',17),(21,8,87,4,'2025-04-06','aceptada',23);
 /*!40000 ALTER TABLE `reserve` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,7 +124,7 @@ CREATE TABLE `restaurant` (
   UNIQUE KEY `username_UNIQUE` (`email`),
   KEY `food_id` (`food_id`),
   CONSTRAINT `restaurant_ibfk_1` FOREIGN KEY (`food_id`) REFERENCES `food` (`food_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,7 +133,7 @@ CREATE TABLE `restaurant` (
 
 LOCK TABLES `restaurant` WRITE;
 /*!40000 ALTER TABLE `restaurant` DISABLE KEYS */;
-INSERT INTO `restaurant` VALUES (13,'La Cocina','lacocina@gmail.com','scrypt:32768:8:1$c3BaMzV6ykU7tJEl$995d41251acb3f53509bda02a8918cb1a1e3118a267da48c4a2602c399c51d12588e0fd730881506c26716712f716f8dd2a44d0c120cfdea1c9efc5b4490497f','Calle Mayor 12',40,'600600600','Restaurante mediterráneo con mariscos frescos.','Paella de mariscos','www.lacocina.com	','',1),(14,'La Trattoria','latrattoria@email.com','scrypt:32768:8:1$I0p4O9fFzeD1oCy4$c9e5966c6b94c90c88aaef2fd618ee943106b96ab3eeb9dcf9c4e4455a2153e8d7a62b2f4c3c8c68dff147f060a13223bf374a12f290a20d3157022493de8c06','Calle del Mar, 25, 36201 Vigo, Pontevedra, España',28,'456987123','Comida muy rica','pizza mediterranea','www.latrattoria.com',NULL,1),(15,'Il Napolitano','ilnapolitano@gmail.com','scrypt:32768:8:1$HVd9BJTGNCFFER1r$b4ff6fa09827de213a5299b1eef8b4a38a220c574d6fa6c2207cba15939a6a8478e2c141bde26227f13dabde18c0af566e75155d642ca41359ae63d180dd3f69','C/Mestre Chané Nº12',35,'444444444','sfadsgfdsgfdhhadfhadh h dgadhdfh','spaguetty','www.ilnapolitano.com',NULL,1);
+INSERT INTO `restaurant` VALUES (17,'El Olivo Dorado','elolivodorado@gmail.com','scrypt:32768:8:1$VXZPbSbEHgbT0ag4$8cc9c3c0530c36ad8da8fe89d5b25b6f3ad2398661fc9cfef8c07b5007631be47e26cd0101de3fbc5a80ade47dcbc85d08f5dff312ec2adfbc206636d21e830b','Rúa do Príncipe 123, Vigo, España',25,'+34 986 456 78','Un rincón acogedor en el corazón de Vigo, famoso por su paella de mariscos y tapas tradicionales.','Paella de mariscos y pulpo a la gallega.',' www.elolivodorado.com','El_olivo_dorado.jpeg',1),(18,'La Brisa Azul','labrisaazul@gmail.com','scrypt:32768:8:1$0NIOsEWD6tTNwC0c$cccac3f995848f41c2f4b3485bb5e92b39789255580455b20fb51533f5b54ac027ac2d0d1055e074d6cde36015155f44c873de27da7af888d47ad0a3db318835','Avenida Samil 45, Vigo, España',30,'+34 986 234 56','Un restaurante con vistas a la Ría de Vigo, especializado en pescados frescos y ensaladas saludables.','Lubina a la sal y ensalada griega.',' www.labrisaazul.com','la_brisa_azul.png',1),(19,'Terra & Mar','terra&mar@gmail.com','scrypt:32768:8:1$JZvwnNHZAidpoDYY$f37f9372b9720ea5a8645723d9c3e4ac7ea21b60658fa151c26a7e7c1b3eab8031f663a7b9486779dc57af1108e7c96c7f0f5b7409b2fa3e3539b51f62a07094','Praza da Constitución 8, Vigo, España',20,'+34 986 789 45','Un elegante restaurante que fusiona lo mejor del mar y la tierra con ingredientes frescos de la región.','Risotto de mariscos y carpaccio de atún.',' www.terramar.es','terramar.png',1),(20,'Sirocco Taverna','siroccotaverna@gmail.com','scrypt:32768:8:1$hw5hhzsqLrxVx1RS$de8e4ef0fe23d0d83ea652b8c625c1fa736238c0b2f7a6c53e328c5617e6e98a32405dae55284501fb6808c72817b41d77a8454231f9b7031566afc67df63e10','Rúa da Ostrería 22, Vigo, España',25,'+34 986 987 65','Una taberna mediterránea con un ambiente cálido, perfecta para disfrutar de auténticas mezedes y vinos locales.','Moussaka y souvlaki de cordero.','www.siroccotaverna.es','siroco_taverna.png',1),(21,'Costa del Sabor','costadelsabor@gmail.com','scrypt:32768:8:1$J4RYE29UKFZmBp8n$702930b289b83db16343decac11e42aad546fb0f802b1602266b9ee201fc73bcc3d9f2e278cf23e6b05242269f53d8d99c764daa6b227f8a8a47f29943220ba2','Paseo de Alfonso XII 77, Vigo, España',18,'+34 986 321 78','Un lugar ideal para disfrutar de la auténtica comida gallega con un toque mediterráneo y vistas a la ría.','Gazpacho andaluz y boquerones en vinagre.',' www.costadelsabor.com','Costa_del_Sabor.jpeg',1),(22,'La Bodega del Puerto','labodegadelpuerto@gmail.com','scrypt:32768:8:1$XQ9qOYT0ERxGMdm5$f031044113b0eb6e857f17caf2b08995ec47f03b4b0ddcd6b0f57cd7ab3f5e03b5e1086e613d2638c5d9f1d95767fc077077434ff3916858efbfbaed704fda5a','Calle del Arenal, 15, 36201 Vigo',20,'986 12 34 56','Un acogedor restaurante con una amplia selección de vinos españoles y tapas tradicionales.','Marisco y Paella.','www.labodegadelpuertovigo.com','bodegapuerto.png',2),(23,'El Rincón de Galicia','elrincondegalicia@gmail.com','scrypt:32768:8:1$blQIisWTENEGr6pv$85d733dfb4a3ea641057c8275fa6af06528a445821f2bdbfe9715e00212598be4d0429f37d8e3f05edf21df3c5e276b53b59c4763ca08f4127e458208f2fca53','Plaza de Compostela, 8, 36202 Vigo',20,'986 65 43 21','Un restaurante familiar que ofrece platos gallegos auténticos con un toque moderno. ','Pulpo a la Gallega y Carnes a la Brasa.','www.elrincondegavigo.es','rincongalicia.png',2),(24,'Casa de Tapas Don Pepe','casadetapasdonpepe@gmail.com','scrypt:32768:8:1$vJDyHYY7abk803dI$a6036a8542053e24d34f4ac22446de96b540a0903e3c25b08598bd0c359821344fc4cc9f24c6f71fd3bf20090f47ce4ab144dd2ddfcbacba4c353f232ccf8ea8','Calle Príncipe, 22, 36202 Vigo.',30,'986 98 76 54','Un lugar animado y bullicioso donde se pueden degustar una gran variedad de tapas españolas.','Croquetas y Tortilla Española.','www.casadetapasdonpepe.es','tapasdonpepe.png',2),(25,'El Mesón Castellano','elmesoncastellano@gmail.com','scrypt:32768:8:1$zXj60eBMjcHwILWg$72602f0414ee83ac1171db06dfc4e2d8699c277a1a07772cd0df7b3859352e127e1ef857046da752d65eed9a4c09c0523e9f104517c312ab783b92c99e415ca3','Calle Colón, 5, 36201 Vigo.',50,'986 45 67 89','Un restaurante elegante que ofrece platos de la cocina castellana con una presentación cuidada. ','Cochinillo y Cordero.',' www.elmesoncastellano.com','mesoncastellano.png',2),(26,'La Paella de la Abuela','lapaelladelaabuela@gmail.com','scrypt:32768:8:1$5jUKCmfdXFsT70pj$83e1cea197fd92eb8485300aaa99bff0ff899119885fc43675569e6425a6c7d33aa93c99187dc593ab22f781e83b77fa9de20b37b15ff04d05abe9fbddf97e8d','Calle Rosalía de Castro, 10, 36202 Vigo.',30,'986 78 90 12','Un restaurante especializado en paellas y arroces, con recetas tradicionales transmitidas de generación en generación. Ambiente acogedor y familiar.','Paellas y Arroces.',' www.lapaelladelaabuela.com.','paellaabuela.png',2);
 /*!40000 ALTER TABLE `restaurant` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,7 +154,7 @@ CREATE TABLE `time_slot` (
   KEY `restaurant_id` (`restaurant_id`),
   KEY `fk_reserva` (`reserve_id`),
   CONSTRAINT `fk_reserva` FOREIGN KEY (`reserve_id`) REFERENCES `reserve` (`reserve_id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=108 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -163,7 +163,7 @@ CREATE TABLE `time_slot` (
 
 LOCK TABLES `time_slot` WRITE;
 /*!40000 ALTER TABLE `time_slot` DISABLE KEYS */;
-INSERT INTO `time_slot` VALUES (24,13,'12:00:00','13:00:00',NULL),(25,13,'13:00:00','14:00:00',NULL),(26,13,'14:00:00','15:00:00',NULL),(27,13,'20:00:00','21:00:00',NULL),(28,13,'21:00:00','22:00:00',NULL),(29,13,'22:00:00','23:00:00',NULL),(30,14,'12:00:00','13:00:00',NULL),(31,14,'13:00:00','14:00:00',NULL),(32,14,'14:00:00','15:00:00',NULL),(33,14,'15:00:00','16:00:00',NULL),(34,14,'16:00:00','17:00:00',NULL),(35,15,'13:00:00','14:00:00',NULL),(36,15,'14:00:00','15:00:00',NULL),(37,15,'15:00:00','16:00:00',NULL);
+INSERT INTO `time_slot` VALUES (38,16,'12:00:00','13:00:00',NULL),(39,16,'13:00:00','14:00:00',NULL),(40,16,'14:00:00','15:00:00',NULL),(41,16,'15:00:00','16:00:00',NULL),(42,16,'20:00:00','21:00:00',NULL),(43,16,'21:00:00','22:00:00',NULL),(44,16,'22:00:00','23:00:00',NULL),(45,17,'13:00:00','14:00:00',NULL),(46,17,'14:00:00','15:00:00',NULL),(47,17,'15:00:00','16:00:00',NULL),(48,17,'20:00:00','21:00:00',NULL),(49,17,'21:00:00','22:00:00',NULL),(50,17,'22:00:00','23:00:00',NULL),(51,18,'12:30:00','13:30:00',NULL),(52,18,'13:30:00','14:30:00',NULL),(53,18,'14:30:00','15:30:00',NULL),(54,18,'15:30:00','16:30:00',NULL),(55,18,'20:00:00','21:00:00',NULL),(56,18,'21:00:00','22:00:00',NULL),(57,18,'22:00:00','23:00:00',NULL),(58,19,'13:00:00','14:00:00',NULL),(59,19,'14:00:00','15:00:00',NULL),(60,19,'15:00:00','16:00:00',NULL),(61,19,'20:30:00','21:30:00',NULL),(62,19,'21:30:00','22:30:00',NULL),(63,19,'22:30:00','23:30:00',NULL),(64,20,'12:00:00','13:00:00',NULL),(65,20,'13:00:00','14:00:00',NULL),(66,20,'14:00:00','15:00:00',NULL),(67,20,'15:00:00','16:00:00',NULL),(68,20,'20:00:00','21:00:00',NULL),(69,20,'21:00:00','22:00:00',NULL),(70,20,'22:00:00','23:00:00',NULL),(71,21,'13:00:00','14:00:00',NULL),(72,21,'14:00:00','15:00:00',NULL),(73,21,'15:00:00','16:00:00',NULL),(74,21,'20:00:00','21:00:00',NULL),(75,21,'21:00:00','22:00:00',NULL),(76,21,'22:00:00','23:00:00',NULL),(77,22,'13:00:00','14:00:00',NULL),(78,22,'14:00:00','15:00:00',NULL),(79,22,'15:00:00','16:00:00',NULL),(80,22,'20:00:00','21:00:00',NULL),(81,22,'21:00:00','22:00:00',NULL),(82,22,'22:00:00','23:00:00',NULL),(83,23,'13:30:00','14:30:00',NULL),(84,23,'14:30:00','15:30:00',NULL),(85,23,'15:30:00','16:30:00',NULL),(86,23,'21:00:00','22:00:00',NULL),(87,23,'22:00:00','23:00:00',NULL),(88,23,'23:00:00','00:00:00',NULL),(89,24,'12:00:00','13:00:00',NULL),(90,24,'13:00:00','14:00:00',NULL),(91,24,'14:00:00','15:00:00',NULL),(92,24,'20:30:00','21:30:00',NULL),(93,24,'21:30:00','22:30:00',NULL),(94,24,'22:30:00','23:30:00',NULL),(95,25,'13:00:00','14:00:00',NULL),(96,25,'14:00:00','15:00:00',NULL),(97,25,'15:00:00','16:00:00',NULL),(98,25,'21:00:00','22:00:00',NULL),(99,25,'22:00:00','23:00:00',NULL),(100,25,'23:00:00','00:00:00',NULL),(101,26,'12:30:00','13:30:00',NULL),(102,26,'13:30:00','14:30:00',NULL),(103,26,'14:30:00','15:30:00',NULL),(104,26,'15:30:00','16:30:00',NULL),(105,26,'21:00:00','22:00:00',NULL),(106,26,'22:00:00','23:00:00',NULL),(107,26,'23:00:00','00:00:00',NULL);
 /*!40000 ALTER TABLE `time_slot` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -176,4 +176,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-01  1:31:59
+-- Dump completed on 2025-04-11  0:15:48
